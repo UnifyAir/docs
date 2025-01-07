89616dd0f860f78eb8436cf91d256ab96a8d8cf0
Pranav Kumar <pcom118@gmail.com>
2025-01-07 13:09:33 +0530

    adding cname gen script

--- cname.sh
+++ cname.sh
@@ -1,8 +1,12 @@
+DEPLOY_BRANCH="gh-pages"
 git stash
+git fetch gh-pages
 git checkout gh-pages
+git pull origin gh-pages
 echo "docs.unifyair.com" >CNAME
 git add CNAME
 git commit -m "Create CNAME"
 git push origin
+git branch -D gh-pages
 git checkout -
-git unstash
+git stash pop
